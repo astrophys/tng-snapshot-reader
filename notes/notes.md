ef0d6a4f32aacbf395121b5bb258c9e9923b9aab
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2024-02-13 14:12:10 -0500

    Summary :    From previous commits, I suspect that I'm getting the axis wrong, so I worked on plotting    the halos and gas density side-by-side.  Turns out that I need to sum along axis=0 for the    gas density. I need to apply this information to the plotting in    collate_segmentation_results.py
    heatmap.py -> src/heatmap.py
    
    notes/notes.md :
    1. Added notes so I can keep track of my work
    
    notes/images/ :
    1. Images mentioned in my notes
    2. halo_vs_gas_npsum_axis0.jpg : shows that I need to sum along axis=0
    3. segmentation_with_halos.png : shows that my halos vs. segmentation is subpar, mentioned
       in previous commit
    
    src/plot_halo_pos_mass_npz.py :
    1. Substantially reworked this code so I can plot halos and gas density together
    2. Turns out that I need to sum along np.sum(density, axis=0) in order to plot the
       halos side-by-side the gas density. See halo_vs_gas_npsum_axis0.jpg.
    3. B/c there are a lot of halos, I only do the plotting along a 10% slice from z=0-7500kpc/h

---
+++ notes/notes.md
@@ -0,0 +1,69 @@
+<!--
+Compile :
+    pandoc -f markdown notes/somefile.md - -filter pandoc-crossref -t latex -o somefile.pdf
+
+Notes:
+    1. http://lierdakil.github.io/pandoc-crossref/
+    #. On over/under braces : https://tex.stackexchange.com/a/132527/84495
+-->
+
+
+<!--
+    YAML section
+-->
+---
+title: Notes on developing tng-snapshot-reader
+author: Ali Snedden
+date: 2024-02-01
+abstract:
+...
+---
+header-includes:
+  - \hypersetup{colorlinks=true,
+            urlcolor=blue,
+            pdfborderstyle={/S/U/W 1}}
+    \usepackage{mathtools}
+    \usepackage{cancel}
+---
+\definecolor{codegray}{gray}{0.9}
+\newcommand{\code}[1]{\colorbox{codegray}{\texttt{#1}}}
+<!-- \let\overbracket\overbracket[0.2mm][0.6mm]{#1}      not sure that this works -->
+
+\maketitle
+\tableofcontents
+\pagebreak
+
+
+13-feb-2024 :
+======================================
+1. Cleaned up duplicate files in top level dir on CRC
+    a) There are copies in output/
+        #. star_snap_135_nvox_100.pkl
+        #. dm_snap_135_nvox_100.pkl
+        #. gas_snap_135_nvox_100.pkl
+#. In previous commits, I'd started adding the ability to output the halos in a format
+   that Disperse would like. 
+    a) This is a non-starter b/c I had real difficulty getting CGAL built so I can
+       actually run his code. This is a nightmare b/c the software is so old that it
+       is going to be darn near impossible to rebuild
+        #. Likely will turn into dependency hell
+#. Also, I'd like to build the ability to see what kind of halos are in what kind
+   of structure. 
+    a) First pass using plot.py, doesn't seem promising
+        #. Ran w/
+            * python -m pdb src/collate_segmentation_results.py --data snap_099/data/halo_pos_mass.npz  --vessels /afs/crc.nd.edu/group/phillips/software/SEGMENT_das/2.0/data/2almost3_donut_matlab_int.txt --clusters snap_099/output/clusters/vesselness.txt --voids snap_099/output/voids/vesselness.tx
+            * Takes 5-10 min to run, so iteration time is terrible, I need a smaller 
+              set to work on.
+            * The halos don't take much time to read, but the 600^3 volumes do
+        #. See : segmentation_with_halos.png
+            * The axis need flipped, but still looks like bad
+            * I wonder if this is a bug on the segmentation part or if its the
+              visualization part
+#. Let's read in both the halos and gas and hold side-by-side to see that they
+   make sense
+    a) Did this with src/plot_halo_pos_mass_npz.py
+    #) Turns out that plotting subDF['x'], subDF['y'] (halos) maps exactly to 
+       np.sum(density[0:gasthresh,:,:], axis=0)
+        #. CONCLUSION : Yes my axis are flipped. 
+            * See halo_vs_gas_npsum_axis0.jpg
+
--- src/plot_halo_pos_mass_npz.py
+++ src/plot_halo_pos_mass_npz.py
@@ -3,6 +3,8 @@
 # 
 #
 #
+"""Code to plot dark matter halos from TNG simulations with [optionally] gas density together
+"""
 import sys
 import argparse
 import numpy as np
@@ -19,44 +21,89 @@ def main():
     Raises:
     """
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument('--data', metavar='halo_pos_mass.npz', type=str,
+    parser.add_argument('--halos', metavar='halo_pos_mass.npz', type=str,
                         help='Input npy file')
-    parser.add_argument('--output', metavar='output', type=str,
-                        help='')
+    parser.add_argument('--density', metavar='halo_pos_mass.npz', type=str, nargs='?',
+                        help='Input npy file')
+    #parser.add_argument('--output', metavar='output', type=str,
+    #                    help='')
     args = parser.parse_args()
-    npz = np.load(args.data, allow_pickle=True)
-    dataV = npz['data']         # This is an ugly np.vector of tuples, let's rework this
-    # Put the dataV into a data frame that I can more easily handle
-    dataL = []
-    for i in range(dataV.shape[0]):
-        #lst = [dataV[i][0], dataV[i][1], dataV[i][2], dataV[i][3], dataV[i][4]]
-        dataL.append(list(dataV[i]))
-    df = pd.DataFrame(dataL)
+    halos = np.load(args.halos, allow_pickle=True)
+    density = np.load(args.density, allow_pickle=True)
+
+    ### Work on Halos
+    haloV = halos['data']         # This is an ugly np.vector of tuples, let's rework this
+    # Put the haloV into a data frame that I can more easily handle
+    haloL = []
+    for i in range(haloV.shape[0]):
+        #lst = [haloV[i][0], haloV[i][1], haloV[i][2], haloV[i][3], haloV[i][4]]
+        haloL.append(list(haloV[i]))
+    haloDF = pd.DataFrame(haloL)
     # Strip out spaces, make lower case
-    header = [string.split(' ')[0].lower() for string in npz['metadata'].item()['headers']]
-    units = npz['metadata'].item()['units']
+    header = [string.split(' ')[0].lower() for string in halos['metadata'].item()['headers']]
+    units = halos['metadata'].item()['units']
     
-    df.columns = header
-    # Only plot from 0 -> zthresh
-    zthresh = 1000     # kpc/h
-    subDF = df[df['z'] < zthresh]
-    #slab = 
+    haloDF.columns = header
+    ### Work on density
+
 
     fig = plt.figure()
-    gs = fig.add_gridspec(1,1)
-    ax = fig.add_subplot(gs[0,0])
-    ax.scatter(subDF['x'], subDF['y'], s=np.log10(np.max(subDF['mass'])+2)/10000)
-    ax.set_title("{:<.2f}kpc/h slab in z-axis".format(zthresh))
+    # Only plot halos
+    if density is None:
+        # Only plot from 0 -> zthresh
+        zthresh = 1000     # kpc/h
+        print("Subsetting halos from 0 -> {}kpc/h".format(zthresh))
+        subDF = haloDF[haloDF['z'] < zthresh]
+        #slab = 
+        gs = fig.add_gridspec(1,1)
+        ax = fig.add_subplot(gs[0,0])
+        ax.scatter(subDF['x'], subDF['y'], s=np.log10(np.max(subDF['mass'])+2)/10000)
+        ax.set_title("{:<.2f}kpc/h slab in z-axis".format(zthresh))
+    else:
+        # Let's plot z=0 -> 7500kpc/h, it may be the issue that the order of axis in
+        # the gas file is off
+        # Hard coded for now
+        halothresh = 7500
+        gasthresh  = 60
+        subDF = haloDF[haloDF['z'] < halothresh]
+        gs = fig.add_gridspec(1,2)
+        # halos
+        ax = fig.add_subplot(gs[0,0])
+        #ax.scatter(haloDF['x'], haloDF['y'], s=np.log10(np.max(haloDF['mass'])+2)/1500000)
+        ax.scatter(subDF['x'], subDF['y'], s=np.log10(np.max(subDF['mass'])+2)/10000)
+        ax.set_title("{:<.2f}kpc/h slab in z-axis".format(halothresh))
+        # density
+        ax = fig.add_subplot(gs[0,1])
+        # Summing over axis=2 really is over z-axis, to test
+        # aa = np.asarray([[[1,2],[3,4],[5,6]], [[7,8],[9,10],[11,12]], [[13,14],[15,16],[17,18]], [[19,20],[21,22],[23,24]]])
+        # (Pdb) aa.shape
+        # (4, 3, 2)
+        # (Pdb) np.sum(aa, axis=2).shape
+        # (4, 3)
+        #density2D = np.sum(density[:,:,0:gasthresh], axis=2)
+        #density2D = np.sum(density[:,0:gasthresh,:], axis=1
 
-    print('Subset = {}'.format(subDF.shape))
+        # This gets the correct spatial organization of Gas density w/r/t Halos)
+        #   --> See halo_vs_gas_npsum_axis0.jpg
+        density2D = np.sum(density[0:gasthresh,:,:], axis=0)
+        im = ax.imshow(np.log10(density2D+1))
+        fig.colorbar(im, ax=ax, anchor=(0, 0.3), shrink=0.7)
+        ax.set_xlim(0,density.shape[0])
+        ax.set_ylim(0,density.shape[1])
+        ax.set_title("0-{} gas slab in z-axis".format(gasthresh))
+        fig.suptitle('z=[0-{}kpc/h]'.format(halothresh))
+
+
+    #print('Subset = {}'.format(subDF.shape))
     plt.show()
 
-    fout = open("{}.NDfield_ascii".format(args.output), "w+")
-    # Header
-    fout.write("ANDFIELD COORDS\n")
-    fout.write("3 {}\n".format(subDF.shape[0]))
-    for index, row in subDF.iterrows():
-        fout.write("{} {} {}\n".format(row['x'], row['y'], row['z']))
+    ### Obsolete until (if ever) figure out Disperse
+    #fout = open("{}.NDfield_ascii".format(args.output), "w+")
+    ## Header
+    #fout.write("ANDFIELD COORDS\n")
+    #fout.write("3 {}\n".format(subDF.shape[0]))
+    #for index, row in subDF.iterrows():
+    #    fout.write("{} {} {}\n".format(row['x'], row['y'], row['z']))
     #fout.write("%i %i %i\n"%(nx,ny,nz))
     #for k in range(0, nz):
     #    for j in range(0, ny):
@@ -64,7 +111,7 @@ def main():
     #            fout.write("{:<.4e} ".format(data[i,j,k]))
     #        fout.write("\n")
     #print("Finished outputting : %s"%(args.output))
-    fout.close()
+    #fout.close()
     sys.exit(0)
 
 
